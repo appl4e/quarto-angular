e76acf16c9eeaea731fc1dd755e878a281c3fc5f
apple <apple@redlimesolutions.com>
2018-09-11 14:16:05 +0600

    component folder separated

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { HomeComponent } from './home/home.component';
+
+import { NgSelectModule } from '@ng-select/ng-select';
+import { OwlModule } from '../../../node_modules/ngx-owl-carousel';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgSelectModule,
+    OwlModule
+  ],
+  declarations: [HomeComponent],
+  exports: [
+    HomeComponent
+  ]
+})
+export class ComponentsModule { }
--- src/app/layout/dashboard-layout/dashboard-layout.module.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { DashboardLayoutRoutes } from './dashboard-layout.routing';
-import { HomeComponent } from '../home/home.component';
+
 
 import { NgSelectModule } from '@ng-select/ng-select';
 import { OwlModule } from '../../../../node_modules/ngx-owl-carousel';
+import { HomeComponent } from '../../components/home/home.component';
 
 
 

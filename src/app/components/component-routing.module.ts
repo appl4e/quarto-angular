5afbe1f8be517ca5b6266ee514d3786844a40232
apple <apple@redlimesolutions.com>
2018-09-18 15:00:10 +0600

    Edit document and view document pages created

--- src/app/components/component-routing.module.ts
+++ src/app/components/component-routing.module.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '@app/components/home/home.component';
 import { CreateDocument } from '@app/components/document/create-document/create-document.component';
+import { EditDocument } from '@app/components/document/edit-document/edit-document.component';
+import { ViewDocument } from '@app/components/document/view-document/view-document.component';
 
 
 
 export const ComponentRoutes: Routes = [
   {path: '', component: HomeComponent, data: {title: 'Home'}},
-  { path: 'create-document', component: CreateDocument, data: {title: 'Create Document'}}
+  { path: 'create-document', component: CreateDocument, data: {title: 'Create Document'}},
+  { path: 'edit-document', component: EditDocument, data: {title: 'Edit Document'}},
+  { path: 'view-document', component: ViewDocument, data: {title: 'Edit Document'}},
 ]
--- src/app/components/document/document.module.ts
+++ src/app/components/document/document.module.ts
@@ -7,6 +7,8 @@ import { CreateDocument } from './create-document/create-document.component';
 import {FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SortablejsModule } from 'node_modules/angular-sortablejs';
+import { EditDocument } from './edit-document/edit-document.component';
+import { ViewDocument } from './view-document/view-document.component';
 
 
 @NgModule({
@@ -20,6 +22,6 @@ import { SortablejsModule } from 'node_modules/angular-sortablejs';
     FroalaViewModule,
     SortablejsModule
   ],
-  declarations: [CreateDocument]
+  declarations: [CreateDocument, EditDocument, ViewDocument]
 })
 export class DocumentModule { }

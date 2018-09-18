71eae95d2d077303a4c8de443a275a58f47bfd99
apple <apple@redlimesolutions.com>
2018-09-18 17:09:34 +0600

    document view page 65% complete

--- src/app/components/document/view-document/view-document.component.ts
+++ src/app/components/document/view-document/view-document.component.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewDocument implements OnInit {
 
+  isCollapsed = false;
+
+  // Defining sections array
+  sections = [
+    { title: 'Introduction', description: '', id: 1, completed: false },
+    { title: 'How to start', description: 'Description of How to start', id: 2, completed: false },
+    { title: 'Topic Body', description: 'Description of Topic Body', id: 3, completed: false },
+    { title: 'Conclusion', description: 'Description of Conclusion', id: 4, completed: false }
+  ];
+
   constructor() { }
+  
+
+  collapse(){
+    this.isCollapsed = !this.isCollapsed;
+    return false;
+  }
 
   ngOnInit() {
   }
